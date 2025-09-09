580694bfa275aeae19d2aada508378ee85eb0e2e
unknown <james.zambrano1@utp.ac.pa>
2025-09-08 20:06:16 -0500

    Parcial 1

--- PARCIALES/PARCIAL_1/operaciones_cadenas.php
+++ PARCIALES/PARCIAL_1/operaciones_cadenas.php
@@ -1,13 +1,20 @@
 <?php
-function contar_palabras_repetidas($texto){
-    foreach ($texto as $frase) {
-        
-    }
-}
-
-function capitalizar_palabras( $texto ){
-    foreach ($texto as $frase) {
+function contar_palabras_repetidas($texto)
+{
+    strtolower($texto);
+    $palabras = explode(" ", $texto);
+    foreach ($palabras as $palabra) {
+        $contador = 1;
 
     }
 }
-?>
+
+function capitalizar_palabras($texto)
+{
+    strtolower($texto);
+    $palabras = explode(" ", $texto);
+    for ($i = 0; $i < count($palabras); $i++) {
+        strtoupper(substr($palabras[$i],0,1));
+    }
+    return implode(" ", $palabras);
+}
--- PARCIALES/PARCIAL_1/procesador_de_frases.php
+++ PARCIALES/PARCIAL_1/procesador_de_frases.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parcial 1</title>
-</head>
-<body>
-    
-</body>
-</html>
-
 <?php
 include "operaciones_cadenas.php";
 
+$frases = [
+    'Frase 1' => 'Dos por Uno es Dos',
+    'Frase 2' => 'Ocho por Ocho no es Ocho',
+    'Frase 3' => 'Las rosas no son rosas',
+    'Frase 4' => 'El vino tinto es un buen vino',
+];
 
-?>
+foreach ($frases as $clave => $valor) {
+    //contar_palabras_repetidas($valor);
+    $frases_capitalizadas = capitalizar_palabras($valor);
+    print_r($frases_capitalizadas);
+}
